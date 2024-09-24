0dc376254f3f99e8fa75ddc0f532fb8442e1ce25
pradeep <pradeep02012005@gmail.com>
2024-09-24 22:29:20 +0530

    update in home page

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,42 +1,10 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import styles from './UploadPage.module.css'; // Import the CSS module
 
 function UploadPage({ setResumeData }) {
-    const [file, setFile] = useState(null);
     const [loading, setLoading] = useState(false); // Loading state
     const navigate = useNavigate();
-
-    const handleFileChange = (e) => {
-        setFile(e.target.files[0]);
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (!file) {
-            console.error('No file selected!');
-            return;
-        }
-
-        const data = new FormData();
-        data.append('pdf_doc', file);
-        setLoading(true); // Set loading to true when upload starts
-
-        try {
-            const response = await axios.post('http://localhost:8000/process', data, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            setResumeData(response.data); // Set parsed resume data
-            navigate('/display'); // Navigate to display page
-        } catch (error) {
-            console.error('Error parsing the resume!', error);
-        } finally {
-            setLoading(false); // Reset loading state regardless of success or failure
-        }
-    };
 
     const handleBuildFromScratch = () => {
         setResumeData({}); // Set empty resume data
@@ -48,21 +16,10 @@ function UploadPage({ setResumeData }) {
             {loading ? (
                 <div className={styles.loadingScreen}>
                     <div className={styles.spinner}></div>
-                    <p>Processing your resume, please wait...</p>
+                    <p>Processing, please wait...</p>
                 </div>
             ) : (
                 <div className={styles.splitContainer}>
-                    {/* Left side - Upload Section */}
-                    <div className={styles.uploadContainer}>
-                        <h2>Upload Your Resume</h2>
-                        <form onSubmit={handleSubmit}>
-                            <input type="file" onChange={handleFileChange} />
-                            <div className={`${styles.box}`} onClick={handleSubmit}>
-                                Upload
-                            </div>
-                        </form>
-                    </div>
-
                     {/* Right side - Build from Scratch Section */}
                     <div className={styles.buildContainer}>
                         <h2>Build from Scratch</h2>
